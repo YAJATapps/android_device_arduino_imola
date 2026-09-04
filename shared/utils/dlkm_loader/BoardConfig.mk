d5bf08b6e72846912cc819ea292cf0a342db4225
Yajat Kumar <yajatapps3@gmail.com>
2026-09-04 01:44:08 -0400

    imola: Populate vendor kernel modules load list from live hardware footprint

--- shared/utils/dlkm_loader/BoardConfig.mk
+++ shared/utils/dlkm_loader/BoardConfig.mk
@@ -5,3 +5,7 @@ TARGET_COPY_OUT_SYSTEM_DLKM := system_dlkm
 BOARD_USES_VENDOR_DLKMIMAGE := true
 BOARD_VENDOR_DLKMIMAGE_FILE_SYSTEM_TYPE := erofs
 TARGET_COPY_OUT_VENDOR_DLKM := vendor_dlkm
+
+# Loadable kernel modules for Arduino Uno Q
+include $(DEVICE_PATH)/shared/utils/dlkm_loader/vendor.modules.list.mk
+BOARD_VENDOR_KERNEL_MODULES_LOAD := $(VENDOR_DLKM_KERNEL_MODULES_LIST)
