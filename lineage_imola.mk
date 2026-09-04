8f1f96e312fe1dfa472c0d677060e6878ad58c31
Yajat Kumar <yajatapps3@gmail.com>
2026-09-04 01:23:59 -0400

    imola: Inherit core_64_bit_only and full_base makefiles

--- lineage_imola.mk
+++ lineage_imola.mk
@@ -3,6 +3,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
 # Inherit tablet common Lineage configurations (Wi-Fi only, no telephony/modem)
 $(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
 
