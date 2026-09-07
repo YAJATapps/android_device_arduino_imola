aabcf95f2bf2eca8bd2d2cb3154636f1fd104d8f
Yajat Kumar <yajatapps3@gmail.com>
2026-09-06 20:37:02 -0400

    imola: align Mesa graphics capabilities with Adreno 702
    - Update ro.opengles.version to 196609 (OpenGL ES 3.1)
    - Drop AEP feature XML (A702 has no geometry or tessellation shaders)
    - Set Vulkan feature version to 1.0 (vulkan.version-1_0_3.xml and level-0.xml)
    - Remove legacy wildcard copy loops in favor of Soong prebuilts
    
    Signed-off-by: Yajat Kumar <yajatapps3@gmail.com>

--- shared/graphics/mesa/device.mk
+++ shared/graphics/mesa/device.mk
@@ -21,7 +21,7 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=160 \
     ro.hardware.egl=mesa \
-    ro.opengles.version=196608 \
+    ro.opengles.version=196609 \
     persist.demo.rotationlock=1
 
 PRODUCT_VENDOR_PROPERTIES += \
@@ -30,33 +30,11 @@ PRODUCT_VENDOR_PROPERTIES += \
 
 # Vulkan & GLES permissions
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml \
     frameworks/native/data/etc/android.software.opengles.deqp.level-2022-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml \
     frameworks/native/data/etc/android.hardware.vulkan.compute-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.compute.xml \
-    frameworks/native/data/etc/android.hardware.vulkan.level-1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level.xml \
-    frameworks/native/data/etc/android.hardware.vulkan.version-1_1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version.xml \
+    frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level.xml \
+    frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2021-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
 TARGET_VULKAN_SUPPORT := true
 TARGET_USES_VULKAN := true
-
-# Automatically copy any out-of-tree Mesa prebuilt libraries dropped into vendor
-MESA_PREBUILT_PATH := vendor/arduino/imola/proprietary/vendor
-
-$(foreach f,$(wildcard $(MESA_PREBUILT_PATH)/lib64/egl/*.so), \
-    $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_VENDOR)/lib64/egl/$(notdir $(f))))
-$(foreach f,$(wildcard $(MESA_PREBUILT_PATH)/lib64/hw/*.so), \
-    $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_VENDOR)/lib64/hw/$(notdir $(f))))
-$(foreach f,$(wildcard $(MESA_PREBUILT_PATH)/lib64/dri/*.so), \
-    $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_VENDOR)/lib64/dri/$(notdir $(f))))
-$(foreach f,$(wildcard $(MESA_PREBUILT_PATH)/lib64/*.so), \
-    $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_VENDOR)/lib64/$(notdir $(f))))
-
-$(foreach f,$(wildcard $(MESA_PREBUILT_PATH)/lib/egl/*.so), \
-    $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_VENDOR)/lib/egl/$(notdir $(f))))
-$(foreach f,$(wildcard $(MESA_PREBUILT_PATH)/lib/hw/*.so), \
-    $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_VENDOR)/lib/hw/$(notdir $(f))))
-$(foreach f,$(wildcard $(MESA_PREBUILT_PATH)/lib/dri/*.so), \
-    $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_VENDOR)/lib/dri/$(notdir $(f))))
-$(foreach f,$(wildcard $(MESA_PREBUILT_PATH)/lib/*.so), \
-    $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_VENDOR)/lib/$(notdir $(f))))
