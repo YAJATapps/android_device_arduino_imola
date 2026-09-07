2810bf2b54c630a6c292098d5361ccf6376f8a45
Yajat Kumar <yajatapps3@gmail.com>
2026-09-06 22:01:02 -0400

    imola: add libcld80211 and component override to disable setup wizard
    - Add libcld80211 to Wi-Fi packages to fix libwifi-hal runtime dependency
    - Group all Wi-Fi packages, permissions, and configs strictly in Wi-Fi block
    - Add imola-component-overrides.xml under Disable Setup Wizard section
    
    Signed-off-by: Yajat Kumar <yajatapps3@gmail.com>

--- device.mk
+++ device.mk
@@ -75,18 +75,19 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.app_widgets.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.app_widgets.xml \
     frameworks/native/data/etc/android.software.autofill.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.autofill.xml \
     frameworks/native/data/etc/android.software.picture_in_picture.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.picture_in_picture.xml \
-    frameworks/native/data/etc/android.software.window_magnification.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.window_magnification.xml \
-    frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml \
-    frameworks/native/data/etc/android.hardware.wifi.direct.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.direct.xml \
-    frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml
+    frameworks/native/data/etc/android.software.window_magnification.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.window_magnification.xml
 
-# Wi-Fi (AIDL HAL, Supplicant, Hostapd, & configs)
+# Wi-Fi (AIDL HAL, Supplicant, Hostapd, permissions & configs)
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
     wpa_supplicant \
-    hostapd
+    hostapd \
+    libcld80211
 
 PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.hardware.wifi.direct.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.direct.xml \
+    frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml \
     $(DEVICE_PATH)/wpa_supplicant.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant.conf \
     $(DEVICE_PATH)/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(DEVICE_PATH)/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf
@@ -162,6 +163,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Disable Setup Wizard
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.setupwizard.mode=DISABLED
+
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/etc/imola-component-overrides.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/imola-component-overrides.xml
 
 # Default timezone and locale
 PRODUCT_PROPERTY_OVERRIDES += \
